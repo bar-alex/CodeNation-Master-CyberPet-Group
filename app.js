7d1655e7679d2874afdf3009b25622f65562be64
Eduard Alexandru <eduard.github@gmail.com>
2022-05-04 14:39:39 +0100

    start the app.js file

---
+++ app.js
@@ -0,0 +1,20 @@
+// base-class: creature
+//     stats:
+//         hunger,
+//         energy,
+//         fitness,
+//         attention,
+//     doFeed()
+//     doSleep()
+//     doExercize()
+//     doPet()
+//     doPlayWith()
+//
+// sub-class: cat, dog, bunny, parrot, pet-rock:Rocky, ai-code, robot
+
+
+// the main class
+class Creature{
+
+
+}
